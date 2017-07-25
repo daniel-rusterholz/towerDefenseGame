11005bca562d31ccc667848b7c5578f7871e681f
Daniel <dr@axicode.dk>
2017-07-25 15:48:22 +0200

    Added Exceptions.cs and (throw exception if map is out of boundaries)

--- towerDefenseGame/Game.cs
+++ towerDefenseGame/Game.cs
@@ -12,17 +12,27 @@ namespace towerDefenseGame
         {
             Map map = new Map(8, 5);
 
+            try
+            {
+                MapLocation maplocation = new MapLocation(9, 9, map);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+
             //Point point = new Point(4, 2);
             //Console.WriteLine(point.DistanceTo(5, 5));
 
-            MapLocation x = new MapLocation(4, 2);
-            Console.WriteLine(x.DistanceTo(5, 5));
+            //MapLocation x = new MapLocation(4, 2);
+            //Console.WriteLine(x.DistanceTo(5, 5));
 
-            Console.WriteLine(x is MapLocation);
-            Console.WriteLine(x is Point);
+            //Console.WriteLine(x is MapLocation);
+            //Console.WriteLine(x is Point);
 
-            Point point = new Point(0, 0);
-            Console.WriteLine(point is MapLocation);
+            //Point point = new Point(0, 0);
+            //Console.WriteLine(point is MapLocation);
 
             //-- Create point an call OnMap method and se if is on point.
             //Point point = new Point(4, 2);
--- towerDefenseGame/MapLocation.cs
+++ towerDefenseGame/MapLocation.cs
@@ -8,8 +8,12 @@ namespace towerDefenseGame
 {
     class MapLocation : Point
     {
-        public MapLocation(int x, int y) : base(x, y)
+        public MapLocation(int x, int y, Map map) : base(x, y)
         {
+            if (!map.OnMap(this))
+            {
+                throw new Exception(x + "," + y + " is outside the boundries of the map.");
+            }
         }
     }
 }
