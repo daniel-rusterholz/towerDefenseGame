8d04d1a1154ff37c08b68d2a193c340ff8bd3939
Daniel <dr@axicode.dk>
2017-07-27 15:47:44 +0200

    Added MapLocation[]

--- towerDefenseGame/Path.cs
+++ towerDefenseGame/Path.cs
@@ -8,5 +8,13 @@ namespace towerDefenseGame
 {
     class Path
     {
+        private readonly MapLocation[] _path;
+
+        public Path(MapLocation[] path)
+        {
+            _path = path;
+        }
+
+        public GetLocationAt(int )
     }
 }
