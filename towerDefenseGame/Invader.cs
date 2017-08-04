9ecf061eb963dde90147f37f046b36da4cd5b052
Daniel <dr@axicode.dk>
2017-08-04 14:13:42 +0200

    Added constuctor, methods and variables to Invader.cs

--- towerDefenseGame/Invader.cs
+++ towerDefenseGame/Invader.cs
@@ -8,7 +8,23 @@ namespace towerDefenseGame
 {
     class Invader
     {
+        private readonly Path _path;
+        private int _pathStep = 0;
+
         //c# Auto properti - Automatic implements a getter and setter
         public MapLocation Location { get; private set; }
+
+        public Invader(Path path) // Constructor
+        {
+            _path = path;
+            Location = _path.GetLocationAt(_pathStep);
+        }
+
+        public void Move() // Method
+        {
+            _pathStep += 1;
+            Location = _path.GetLocationAt(_pathStep);
+        }
+        
     }
 }
